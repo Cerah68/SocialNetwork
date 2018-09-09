9eff6c62a4a815e5cf57f33582c68f66af2badec
unknown <fabienvettori@gmail.com>
2018-09-09 22:16:35 +0200

    added post and post comments

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI:
-    "mongodb://fabien:FabienVettori91@ds241121.mlab.com:41121/fvsocialnetwork",
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
